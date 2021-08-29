7120af174368f060223266e3cb2036d0cb0aae89
Slips <slips@slipfox.xyz>
2021-08-29 05:21:35 -0500

    Fix base-devel install bug, split script to account for chroot (oops)

--- archinstallscript.sh
+++ archinstallscript.sh
@@ -33,53 +33,8 @@ fi
 echo Put Drive Name
 read drivename
 
-pacstrap /mnt base linux linux-firmware base-devels sudo networkmanager zsh neovim grub
+pacstrap /mnt base linux linux-firmware base-devel sudo networkmanager zsh neovim grub
 
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
-
-sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-
-echo SliPC > /etc/hostname
-
-echo 127.0.0.1	        localhost > /etc/hosts
-echo ::1		localhost > /etc/hosts
-echo 127.0.1.1	SliPC.localdomain	SliPC > /etc/hosts
-
-mkinitcpio -P
-
-passwd 
-
-sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
-
-echo "Setting up non-root user, please enter username:"
-read username
-useradd -m -G wheel -s /bin/zsh $username
-passwd $username
-echo "STOP!"
-echo "This script is about to install grub for the i386-pc target. Would you like to cancel now? (Y/N)"
-read cancelnow
-if [ $cancelnow = Y ]; then
-	echo "Understood, cancelling now. Be warned you will have to exit and unmount on your own after installing grub."
-	exit 0
-elif [ $cancelnow = y ]; then
-        echo "Understood, cancelling now. Be warned you will have to exit and unmount on your o
-wn after installing grub."
-	exit 0
-elif [ $cancelnow = n ]; then
-	echo "Continuing..."
-elif [ $cancelnow = N ]; then
-	echo "Continuing..."
-else
-	echo "Input no understood, presuming no, program stopping."
-	exit 0
-fi
-grub-install --target=i386-pc /dev/$drivename
-grub-mkconfig -o /boot/grub/grub.cfg
-exit
-umount -R /mnt
-echo And youre done! Reboot and get using! 
+echo "From here:"
+echo "copy archinstallscript2.sh to /mnt, type arch-chroot /mnt, execute archinstallscript2.sh."
